cae3e1f50f63e5d04e88e2109e94038e829e7af0
codebysubhan <subhanali1212w@gmail.com>
2026-07-01 13:45:44 +0500

    functions in go

--- functions.go
+++ functions.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
-func hello(){
+func Hello(){ // the function with a capital letter can be use outside the package but if we don't want that then we use lowercase letters
 	fmt.Println("Hello")
 }
 
+func Add(x int, y int) (bool, int) {
+	x = 4
+	// fmt.Println(x+y)
+	// return true, x+y\
+	if x+y != 0{
+		return true, x+y
+	} else {
+		return false, x+y
+	}
+}
+
 func main (){
-	hello()
+	Hello()
+	two := 2
+	bl,out := Add(two, 3)
+	fmt.Println(reflect.TypeOf(bl), reflect.TypeOf(out))
+	fmt.Println(out, "<-- Returned Value", bl, "<-- boolean value")
+	fmt.Println("two remains 2 because it is copied to x at the runtime so x and two are different varaibles here: ", two)
 }
