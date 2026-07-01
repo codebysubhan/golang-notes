8ed3f5eaec3e849c77893f33d0b16f8a315de992
codebysubhan <subhanali1212w@gmail.com>
2026-07-01 14:11:50 +0500

    modules in go simple example

---
+++ modules/greeting/greeting.go
@@ -0,0 +1,10 @@
+package greeting
+
+
+import (
+	"fmt"
+)
+
+func Greet() {
+	fmt.Println("Hello!! from greeting.go")
+}
---
+++ modules/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	gr "example.com/modules/greeting"
+)
+
+func main(){
+	gr.Greet()
+}
